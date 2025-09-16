ea350c86ddb96ce6c5386f505769b0a465f7a687
carookim <carookim77@gmail.com>
2025-09-16 17:03:04 +0900

    20250916

---
+++ Pyrhon수업내용/20250916/streamlit/s_03.py
@@ -0,0 +1,26 @@
+# session 개념 ^^
+
+# 세션 상태 관리
+
+# import streamlit as st
+# count = 0
+# if st.button('카운트 증가'):
+#     count += 1
+#     st.write('현재 카운트 : ', count)
+# 이렇게만 작성하면 count가 증가하지않는다.
+# Streamlit은 버튼 클릭, 입력값 변경 등 사용자 이벤트가 발생할 때마다
+# 전체 스크립트를 맨 처음부터 다시 실행합니다.
+
+# Streamlit에서 값을 누적하려면
+# 세션 상태(session_state)를 사용해야 해요.
+# 새션을 사용한다는 것은 상태를 유지
+
+import streamlit as st
+if 'count' not in st.session_state: # session_state : Streamlit에서 사용자별로 상태를 저장하는 공간
+    st.session_state.count = 0
+
+if st.button('카운트 증가'):
+    st.write('버튼 클릭됨')
+    st.session_state['count'] += 1 # ^여기서 왜 대괄호 인지
+st.write('현재 카운트:',st.session_state['count'])
+st.json(st.session_state)  # 세션 상태 확인 # ^ json이 뭔지
