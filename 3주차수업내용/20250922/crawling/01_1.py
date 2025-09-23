efba3a09c200768da9a6eda77d083760bf983f8c
carookim <carookim77@gmail.com>
2025-09-23 13:32:02 +0900

    202050923

--- 3주차수업내용/20250922/crawling/01_1.py
+++ 3주차수업내용/20250922/crawling/01_1.py
@@ -1,27 +1,33 @@
+# 공공데이터 자동차 정보조회 관련
+# ^ 데이터 관리가 안된건지 조회가 안된다.
+
 from dotenv import load_dotenv
 import os
 import requests
 # .env 로드
 load_dotenv()
-P_KEY = os.getenv('PUBLIC_DECODE_KEY')
+P_KEY = os.getenv('PUBLIC_KEY')
 print(f'P_KEY : {P_KEY[-10:]}')
-
-# 데이터를 요청 할 주소
-url = f"http://apis.data.go.kr/B553881/newRegistInfoService/newRegistInfoService?serviceKey={P_KEY}"
 
 # 서버에 보낼 데이터(1페이지를 보여달라는 의미로)
 from_data = {
     'serviceKey' : P_KEY,
     'registYy' : 2017,
     'registMt' : 11,
-    'vhctyAsortCode' : '',
+    'vhctyAsortCode' : 1,
     'registGrcCode' : 1,
-    'useFuelCode' : 1,
-    'cnmCode' : '',
+    'useFuelCode' : 2,
+    'cnmCode' : '000004',
     'prposSeNm' : 1,
     'sexdstn' : '남자',
     'agrde' : 3,
-    'dsplvlCode' : 2
+    'dsplvlCode' : 2,
+    'hmmdImpSeNm' : '국산',
+    'prye' : 2010,
 }
-response = requests.post(url,json=from_data)
-print(response.text[:500])
+
+# 데이터를 요청 할 주소
+url = f"http://apis.data.go.kr/B553881/newRegistInfoService/newRegistInfoService?serviceKey={P_KEY}"
+
+response = requests.get(url, params=from_data)
+print(response.text[:1000])
