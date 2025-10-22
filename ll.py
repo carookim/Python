7a2487d499630365ec5118dc380d7c5c38b1fe96
carookim <carookim77@gmail.com>
2025-10-22 18:02:53 +0900

    20252022

---
+++ ll.py
@@ -0,0 +1,10 @@
+# pip install matplotlib
+# pip install scikit-learn
+# pip install seaborn
+# pip install pandas
+# pip install numpy
+
+# !git clone https://github.com/사용자명/저장소명.git
+
+# !git clone https://github.com/pia222sk20/python/blob/main/machin-learing/telecome_churn.ipynb.git
+# git clone -b 브랜치명 --single-branch https://github.com/사용자명/저장소명.git
--- pandas-data-analysis/part7/ll.py
+++
@@ -1,5 +0,0 @@
-# pip install matplotlib
-# pip install scikit-learn
-# pip install seaborn
-# pip install pandas
-# pip install numpy
