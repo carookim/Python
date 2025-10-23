79708fd16ae83615a3cdd42a1cdb500ef90882d6
carookim <carookim77@gmail.com>
2025-10-23 16:36:39 +0900

    20251023

--- ll.py
+++ ll.py
@@ -4,7 +4,9 @@
 # pip install pandas
 # pip install numpy
 
+# 깃허브 다른사람 파일 복사 명령어
 # !git clone https://github.com/사용자명/저장소명.git
+# !git clone -b 브랜치명 --single-branch https://github.com/사용자명/저장소명.git
 
-# !git clone https://github.com/pia222sk20/python/blob/main/machin-learing/telecome_churn.ipynb.git
-# git clone -b 브랜치명 --single-branch https://github.com/사용자명/저장소명.git
+# 에러 목록
+# AttributeError : 파이썬 객체가 요청된 속성(attribute)이나 메서드를 가지고 있지 않을 때 발생하는 에러
